c73b53a2ae06e4b27889eace7f6aa00fddae92c7
jeln666 <je.luis.nascimento@hotmail.com>
2019-08-26 16:00:51 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -18,6 +18,18 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
+
         public void Insert(Seller seller)
         {
             _context.Add(seller);
